5471ab9518adb898674f8892a619db6843132bf3
Henrique Mauler <henriquemauler@gmail.com>
2024-06-28 15:59:34 -0300

    small wipe

--- frontend/jest.config.js
+++
@@ -1,9 +0,0 @@
-const _testRegex = '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$'
-module.exports = {
-    transform: {
-      '^.+\\.tsx?$': 'ts-jest',
-      '^.+\\.ts?$': 'ts-jest',
-    },
-    testRegex: 'src/test/.*\\.(test|spec)?\\.(ts|tsx)$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  };
